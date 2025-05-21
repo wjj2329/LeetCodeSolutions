cc68bb1c3d56b8c177974e136bd447733d7c115b
William Jones <wjj2329@byu.net>
2025-05-20 21:38:09 -0600

    Time: 2 ms (92.20%) | Memory: 45.8 MB (86.52%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MinFallingPathSum(int[][] matrix) {
+        for(int i = matrix.Length-2; i>-1; i--){
+            for(int s = 0; s<matrix[i].Length; s++){
+                int min = 2147483647;
+                if(s!=0){
+                min = Math.Min(min, matrix[i+1][s-1]);
+                }
+                min = Math.Min(min, matrix[i+1][s]);
+                if(s<matrix[i].Length-1){
+                min = Math.Min(min, matrix[i+1][s+1]);
+                }
+                matrix[i][s]+=min;
+            }
+        }
+        int returning = int.MaxValue; 
+        for(int i = 0; i<matrix[0].Length; i++){
+            returning = Math.Min(returning, matrix[0][i]);
+        }
+        return returning;
+    }
+}
