32f0d6415e72092ba004ae0d515ca1ee60e26ebf
William Jones <wjj2329@byu.net>
2025-05-20 21:18:23 -0600

    Time: 0 ms (100.00%) | Memory: 47.1 MB (30.48%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cs
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> ZigzagLevelOrder(TreeNode root) {
+        List<IList<int>>myList = new List<IList<int>>();
+        Queue<TreeNode>myQueue = new Queue<TreeNode>();
+        if(root!=null){
+        myQueue.Enqueue(root);
+        }
+        bool first = true;
+        while(myQueue.Count>0){
+            int row = myQueue.Count;
+            IList<int>temp = new List<int>();
+            for(int i = 0; i<row; i++){
+                TreeNode curr = myQueue.Dequeue();
+                if(curr.left!=null){
+                    myQueue.Enqueue(curr.left);
+                }
+                if(curr.right!=null){
+                    myQueue.Enqueue(curr.right);
+                }
+                if(first){
+                    temp.Add(curr.val);
+                }else{
+                    temp.Insert(0, curr.val);
+                }
+            }
+            first=!first;
+            myList.Add(temp);
+
+            
+        }
+        return myList;
+    }
+}
