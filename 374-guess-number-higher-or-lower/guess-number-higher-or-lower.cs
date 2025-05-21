1c4bd02cdad3e0d55fb9ae1004853d8837012dd0
William Jones <wjj2329@byu.net>
2025-05-20 21:27:31 -0600

    Time: 26 ms (41.86%) | Memory: 29.3 MB (11.32%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cs
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution : GuessGame {
+    public int GuessNumber(int n) {
+        int upper = n;
+        int lower = 0;
+        while(lower<=upper){
+            int num = lower+(upper - lower) /2;
+            Console.WriteLine(num);
+            int result = guess(num);
+            if(result == 0){
+                return num;
+            }
+            if(result == -1){
+                upper =num-1;
+                Console.WriteLine("I Set upper to "+upper);
+            }
+            if(result == 1){
+                lower = num+1;
+                Console.WriteLine("I Set lower to "+lower);
+            }
+        }
+        return -1;
+    }
+}
