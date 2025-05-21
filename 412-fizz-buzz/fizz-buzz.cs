1c08d0d7e9d2432ef4d160e5305ab32fbf7aec9f
William Jones <wjj2329@byu.net>
2025-05-20 21:30:19 -0600

    Time: 6 ms (56.09%) | Memory: 68.5 MB (9.41%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cs
+++ 412-fizz-buzz/fizz-buzz.cs
@@ -2,11 +2,13 @@ public class Solution {
     public IList<string> FizzBuzz(int n) {
         List<string>myArr = new List<string>(n);
         for(int i = 1; i<n+1; i++){
-            if(i%3!=0 && i%5!=0){
+            int mod3 = i%3;
+            int mod5 = i%5;
+            if(mod3!=0 && mod5!=0){
                 myArr.Add(i.ToString());
-            }else if(i%3 == 0 && i%5!=0){
+            }else if(mod3 == 0 && mod5!=0){
                 myArr.Add("Fizz");
-            }else if(i%5 == 0&& i%3!=0){
+            }else if(mod5 == 0&& mod3!=0){
                 myArr.Add("Buzz");
             }else{
                 myArr.Add("FizzBuzz");
