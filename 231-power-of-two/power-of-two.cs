65da7163bd16c0c0275298c088ebe91092aa80e0
William Jones <wjj2329@byu.net>
2025-06-09 18:46:24 -0600

    Time: 0 ms (100.00%) | Memory: 30.8 MB (82.54%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsPowerOfTwo(int n) {
+        if(n==0){
+            return false;
+        }
+        while(n!=1){
+            if(n%2!=0){
+                return false;
+            }
+            n=n/2;
+        }
+        return true;
+    }
+}
